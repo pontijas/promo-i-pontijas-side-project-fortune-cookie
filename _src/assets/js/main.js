15426bfcdcbcf815f9d0ce4a320db0c4f4bae0e7
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-01 22:28:18 +0200

    set add event listener to cookie image and write quote

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log('holiiii');
+
+const cookieBtn = document.querySelector('.js-cookie-btn');
+const cookieQuote = document.querySelector('.js-cookie-quote');
+
+function breakCookie() {
+  cookieQuote.innerHTML = `What is yours, shall come to you.`;
+  console.log('me han clickado');
+}
+
+cookieBtn.addEventListener('click', breakCookie);
