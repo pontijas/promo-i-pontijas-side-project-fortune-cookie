3c303c179af3383ea4fa746b5c058e1f0a72b94d
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-02 14:04:05 +0200

    set content of quotes array

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,16 +3,14 @@
 const cookieBtn = document.querySelector('.js-cookie-btn');
 const cookieQuote = document.querySelector('.js-cookie-quote');
 
-// quotes i like
-// All things are difficult before they are easy.
-// Patience is your alley.
-// Don’t pursue happiness – create it.
-// We become what we do.
+// chinese quotes i like
 // What is yours, shall come to you.
-// If you want the rainbow, you have to tolerate the rain.
+// Patience is your alley.
+// All things are difficult before they are easy.
+// Don’t pursue happiness – create it.
 // The usefulness of a cup is in its emptiness.
-// Success lies in the hands of those who wants it.
-// Big journeys begin with a single step.
+// Success lies in the hands of those who want it.
+// A journey of thousand miles must begin with a single step.
 // We learn little from success, but much from failure.
 // Yet by calling full, you created emptiness.
 // Silence is a source of great strength.
@@ -21,11 +19,51 @@ const cookieQuote = document.querySelector('.js-cookie-quote');
 // A single arrow is easily broken, but not ten in a bundle.
 // One who smile rather than rages is always the stronger.
 // The day you decide to do it, is your lucky day.
+// Act don't react.
+// Every smile is a direct achievement.
+// To see things in the seed, that is genius.
+// One who obtains has little. One who scatters has much.
+// Manifest plainness, embrace simplicity, reduce selfishness, have few desires.
+// All things in the world come from being. And being comes from non-being.
+// Real Knowledge is to know the extent of one's ignorance.
+// What is firmly rooted cannot be pulled out.
+// Stay at the center of the circle and let things take their course.
 
-const arrQuotes = ['What is yours, shall come to you.', 'We become what we do.', 'Patience is your alley.', 'Don’t pursue happiness – create it.'];
+const quotes = [
+  `What is yours, shall come to you.`,
+  `Patience is your alley.`,
+  `All things are difficult before they are easy.`,
+  `Don’t pursue happiness – create it.`,
+  `The usefulness of a cup is in its emptiness.`,
+  `Success lies in the hands of those who want it.`,
+  `A journey of thousand miles must begin with a single step.`,
+  `We learn little from success, but much from failure.`,
+  `Yet by calling full, you created emptiness.`,
+  `Silence is a source of great strength.`,
+  `Tension is who you think you should be. Relaxation is who you are.`,
+  `One who chases two rabbits catches neither.`,
+  `A single arrow is easily broken, but not ten in a bundle.`,
+  `One who smile rather than rages is always the stronger.`,
+  `The day you decide to do it, is your lucky day.`,
+  `Act, don't react.`,
+  `Every smile is a direct achievement.`,
+  `To see things in the seed, that is genius.`,
+  `One who obtains has little. One who scatters has much.`,
+  `Manifest plainness, embrace simplicity, reduce selfishness, have few desires.`,
+  `All things in the world come from being. And being comes from non-being`,
+  `Real Knowledge is to know the extent of one's ignorance.`,
+  `A good traveler has no fixed plans and is not intent on arriving.`,
+  `Stay at the center of the circle and let things take their course.`,
+  `What is firmly rooted cannot be pulled out.`
+];
 
-function breakCookie() {
-  cookieQuote.innerHTML = `<p class="css-cookie-quote">What is yours, shall come to you.</p>`;
+console.log(quotes);
+console.log(quotes[0]);
+console.log(quotes[5]);
+console.log('quotes length', quotes.length);
+
+function breakCookie(quotes) {
+  cookieQuote.innerHTML = `<p class="css-cookie-quote">Holi</p>`;
   console.log('me han clickado');
 }
 
